0f1f1b46e7617b9384fdc5453910a24a41b40222
mujab <mujababdelmawla@gmail.com>
2026-05-03 18:33:54 +0000

    add banking system unit test

--- test_user.py
+++ test_user.py
@@ -1,9 +1,51 @@
-from user import User 
 
-def test_profile_format():
-    user = User(name="mujab")
-    assert user.get_profile() == "Name : mujab"
+from user import BankAccount
+import pytest
+def test_deposit():
+    # ARRANGE
+    account = BankAccount(owner="mujab", balance=1000000)
 
-def test_empty_name():
-    user = User(name=" ")
-    assert "Name : " in user.get_profile()
+    # ACT
+    result = account.deposit(2000000)
+
+    # ASSERT
+    assert result == 3000000
+
+
+def test_withdraw():
+    # ARRANGE
+    account = BankAccount(owner="mujab", balance=1000000)
+    
+    # ACT
+    result = account.withdraw(20000)
+    
+    # ASSERT
+    assert result == 980000
+
+
+def test_get_balance():
+    # ARRANGE 
+    account = BankAccount(owner="mujab", balance=1000000)
+    # ACT 
+    result = account.get_balance()
+    # ASSERT
+    assert result == 1000000
+
+
+
+def test_insufficient_funds():
+    # ARRANGE
+    account = BankAccount(owner="mujab", balance=500)
+    
+    # ACT & ASSERT
+    with pytest.raises(ValueError):
+        account.withdraw(700)  
+
+
+def test_initial_balance():
+    # ARRANGE
+    account = BankAccount(owner="mujab", balance=1000000)
+    # ACT
+    result = account.get_balance()
+    # ASSERT
+    assert result == 1000000
--- user.py
+++ user.py
@@ -1,6 +1,17 @@
-class User:
-    def __init__(self, name):
-        self.name = name 
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
 
-    def get_profile(self):
-        return f"Name : {self.name}"
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("Insufficient funds")
+        self.balance -= amount
+        return self.balance
+
+    def get_balance(self):
+        return self.balance
